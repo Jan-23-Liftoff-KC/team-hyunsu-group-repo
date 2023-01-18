5a393e7d7d28fd017281315f4451bcff0a202b0d
KitPyles <pyles.kit@gmail.com>
2023-01-18 09:56:39 -0600

    Added base of Spring Boot MVC

---
+++ src/test/java/org/launchcode/healthehub/HealthEhubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.healthehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthEhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
