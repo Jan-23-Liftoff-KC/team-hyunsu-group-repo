9e9fc1515c995b294c47f5ba4cbd2b1939df2499
KitPyles <pyles.kit@gmail.com>
2023-01-27 19:56:03 -0600

    Commit df2e146 altered file.

---
+++ src/main/java/org/launchcode/healthehub/HealthEHubApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.healthehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthEHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthEHubApplication.class, args);
+	}
+
+}
