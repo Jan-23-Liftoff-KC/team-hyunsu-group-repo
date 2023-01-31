64a622e42d9b57fb275df7d4c4e93105481a5337
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2023-01-30 21:28:25 -0600

    Delete queries.sql
    Remnant of Java Assignment #4. File not needed.

--- queries.sql
+++
@@ -1,8 +0,0 @@
--- Part 1: Test it with SQL
-SELECT COLUMN_NAME, DATA_TYPE from INFORMATION_SCHEMA. COLUMNS where table_schema = 'user_registration' and table_name = 'user';
--- Part 2: Test it with SQL
-SELECT name FROM employer WHERE location = "St. Louis City";
--- Part 3: Test it with SQL
-DROP TABLE user;
--- Part 4: Test it with SQL
-SELECT * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
