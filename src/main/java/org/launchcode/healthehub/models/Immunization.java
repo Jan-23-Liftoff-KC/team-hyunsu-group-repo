5561ff444de24ea94fe226ce1aa76f5beefc5977
ggarmondeh <glapougbaegarmondeh@gmail.com>
2023-02-04 11:18:41 -0600

    immunization backend

---
+++ src/main/java/org/launchcode/healthehub/models/Immunization.java
@@ -0,0 +1,67 @@
+package org.launchcode.healthehub.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Immunization {
+    @Id
+    @GeneratedValue
+    public int id;
+    private String Name;
+    private String Date;
+    private String Dosage;
+    private String Notes;
+
+    public Immunization(String name, String date, String dosage, String notes) {
+        this.Name = name;
+        this.Date = date;
+        this.Dosage = dosage;
+        this.Notes = notes;
+    }
+    public Immunization(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getDosage() {
+        return Dosage;
+    }
+
+    public void setDosage(String dosage) {
+        Dosage = dosage;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String notes) {
+        Notes = notes;
+    }
+}
+
