c6165249fcae1642d8f4426a80a29513d2184f44
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2023-02-20 14:34:02 -0600

    Fixed import statement

--- src/main/java/org/launchcode/healthehub/models/data/NotesRepository.java
+++ src/main/java/org/launchcode/healthehub/models/data/NotesRepository.java
@@ -1,7 +1,7 @@
 package org.launchcode.healthehub.models.data;
 
 import org.launchcode.healthehub.models.Notes;
-import org.springframework.data.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotesRepository extends JpaRepository <Notes, Integer> {
 }
