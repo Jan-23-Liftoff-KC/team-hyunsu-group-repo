b0c5a3019d5621602406e4da9b49c5e948ab023a
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2023-02-20 14:34:25 -0600

    Fixed import statement

--- src/main/java/org/launchcode/healthehub/models/data/PharmacyRepository.java
+++ src/main/java/org/launchcode/healthehub/models/data/PharmacyRepository.java
@@ -2,7 +2,6 @@ package org.launchcode.healthehub.models.data;
 
 import org.launchcode.healthehub.models.Pharmacy;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
