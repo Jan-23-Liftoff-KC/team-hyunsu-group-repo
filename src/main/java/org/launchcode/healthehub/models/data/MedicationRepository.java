891ded9627bc4fcb4c84b760d0bb8d71db077a2c
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2023-02-20 12:52:52 -0600

    Switch from CrudRepository to JpaRepository

--- src/main/java/org/launchcode/healthehub/models/data/MedicationRepository.java
+++ src/main/java/org/launchcode/healthehub/models/data/MedicationRepository.java
@@ -1,9 +1,9 @@
 package org.launchcode.healthehub.models.data;
 
 import org.launchcode.healthehub.models.Medication;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MedicationRepository extends CrudRepository<Medication,Integer> {
+public interface MedicationRepository extends JpaRepository<Medication,Integer> {
 }
