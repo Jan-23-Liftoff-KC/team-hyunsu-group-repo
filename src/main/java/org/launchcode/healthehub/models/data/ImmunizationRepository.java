a2b98c235c3f80818b4d6f17df85f9c9711b389b
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2023-02-20 14:33:09 -0600

    Fixed typo

--- src/main/java/org/launchcode/healthehub/models/data/ImmunizationRepository.java
+++ src/main/java/org/launchcode/healthehub/models/data/ImmunizationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImmunizationRepository extends JpaRepositoryRepository<Immunization, Integer > {
+public interface ImmunizationRepository extends JpaRepository<Immunization, Integer > {
 }
