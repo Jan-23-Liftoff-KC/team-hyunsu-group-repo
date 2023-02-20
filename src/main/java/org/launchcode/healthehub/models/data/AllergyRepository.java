df5fab2e3f3f0abb70f578383d10c9bc1f990e6a
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2023-02-20 12:52:14 -0600

    Switch from CrudRepository to JpaRepository

--- src/main/java/org/launchcode/healthehub/models/data/AllergyRepository.java
+++ src/main/java/org/launchcode/healthehub/models/data/AllergyRepository.java
@@ -1,9 +1,9 @@
 package org.launchcode.healthehub.models.data;
 
 import org.launchcode.healthehub.models.Allergy;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AllergyRepository extends CrudRepository <Allergy, Integer> {
+public interface AllergyRepository extends JpaRepository <Allergy, Integer> {
 }
