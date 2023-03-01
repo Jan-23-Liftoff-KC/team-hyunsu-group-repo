bc0b509082b3a77e71e00b5fa4993ac25246e804
KitPyles <pyles.kit@gmail.com>
2023-03-01 11:56:55 -0600

    Troubleshooting Dashboard issues

--- src/main/java/org/launchcode/healthehub/models/data/NotesRepository.java
+++ src/main/java/org/launchcode/healthehub/models/data/NotesRepository.java
@@ -1,7 +1,7 @@
 package org.launchcode.healthehub.models.data;
 
-import org.launchcode.healthehub.models.Notes;
+import org.launchcode.healthehub.models.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotesRepository extends JpaRepository <Notes, Integer> {
+public interface NotesRepository extends JpaRepository <Note, Integer> {
 }
